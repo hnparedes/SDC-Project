c2cb29f7012094650d465b9fb6f51ba4ce8f0d33
Maxwell Benefield <maxminecraft109@gmail.com>
2026-04-14 17:44:15 -0400

    Added more error messages for various scenarios that could be encountered while running the archiver program.

--- packages/sdc_common_module/src/sdc_common_module/acm.py
+++ packages/sdc_common_module/src/sdc_common_module/acm.py
@@ -18,11 +18,13 @@ class AccessControlMatrix:
     def add_access_level(self, lvl):
         # Is the name nonempty?
         if not lvl:
-            return False
+            raise Exception("Name cannot be empty. Please input at least one character")
 
         # Are there no existing access levels with this name?
         if lvl in self.access_levels:
-            return False
+            raise Exception(
+                "Access Level with given name already exists. Please choose a different name."
+            )
 
         self.access_levels.append(lvl)
         return True
@@ -32,11 +34,15 @@ class AccessControlMatrix:
     def add_user(self, uid, password, access_level):
         # Are all field nonempty?
         if not uid or not password or not access_level:
-            return False
+            raise Exception(
+                "One or more fields are empty. Please make sure to fill out all fields."
+            )
 
         # Are there no existing users with this uid?
         if uid in self.users:
-            return False
+            raise Exception(
+                "This username already exists in the user list. Please choose a different username."
+            )
 
         self.users[uid] = {
             "password_hash": self.hash_password(password),
@@ -48,14 +54,18 @@ class AccessControlMatrix:
     def add_document(self, doc_id, access_levels):
         # Is the document name and access level list nonempty?
         if not doc_id or not access_levels:
-            return False
+            raise Exception(
+                "One or more fields are empty. Please make sure to fill out all fields."
+            )
 
         # Are there no existing documents with this id?
         # Potential alternate behavior:
         # - Auto-incrementing filenames instead of rejecting duplicate names?
         # - Ask if the user wants to overwrite the existing entry?
         if doc_id in self.documents:
-            return False
+            raise Exception(
+                "This document is already in the SDC Archive. Please choose a different document."
+            )
 
         # TODO: Reject the document if access_levels contains the error access level
 
@@ -65,19 +75,25 @@ class AccessControlMatrix:
     def rename_access_level(self, old_lvl, new_lvl):
         # Is the old level name nonempty and has a corresponding access level?
         if not old_lvl or old_lvl not in self.access_levels:
-            return False
+            raise Exception(
+                "The access level you are trying to rename does not exist. Please choose an existing access level."
+            )
 
         # Is the new level name nonempty?
         if not new_lvl:
-            return False
+            raise Exception("Name cannot be empty. Please input at least one character")
 
         # Is the new level name different from the old level name?
         if new_lvl == old_lvl:
-            return False
+            raise Exception(
+                "The new name is the same as the old one. Please input a new name."
+            )
 
         # Is there no existing access level that already has the new name?
         if new_lvl in self.access_levels:
-            return False
+            raise Exception(
+                "The provided name already exists in the access level list. Please input a new name."
+            )
 
         # Change the access level name
         idx = self.access_levels.index(old_lvl)
@@ -98,18 +114,22 @@ class AccessControlMatrix:
     def update_user(self, old_uid, new_uid, new_pwd, new_lvl):
         # Is the old user ID nonempty and has a corresponding user?
         if not old_uid or old_uid not in self.users:
-            return False
+            raise Exception(
+                "The user you are trying to rename does not exist. Please select an already existing user."
+            )
 
         # Is the new user ID and access level nonempty?
         if not new_uid or not new_lvl:
-            return False
+            raise Exception("Name cannot be empty. Please input at least one character")
 
         # Note that the password is NOT checked for nonemptiness.
         # That's because this function interprets an empty password field as retaining the current password.
 
         # If the user ID is being changed, is the new user ID unique?
         if new_uid != old_uid and new_uid in self.users:
-            return False
+            raise Exception(
+                "The provided username already exists in the user list. Please input a new name."
+            )
 
         old_hash = self.users[old_uid]["password_hash"]
         if new_uid != old_uid:
@@ -124,11 +144,15 @@ class AccessControlMatrix:
     def set_document_perms(self, doc_id, access_levels):
         # Is the document name and access level list nonempty?
         if not doc_id or not access_levels:
-            return False
+            raise Exception(
+                "No access level selected. Please make sure that an entry in the access level list is selected."
+            )
 
         # Does the document we are changing the permissions of exist?
         if doc_id not in self.documents:
-            return False
+            raise Exception(
+                "The document you are trying to edit does not exist. Please select an already existing document."
+            )
 
         # TODO: Reject the document if access_levels contains the error access level
 
@@ -153,11 +177,13 @@ class AccessControlMatrix:
     def delete_access_level(self, lvl_to_delete):
         # Is the level name nonempty?
         if not lvl_to_delete:
-            return False
+            raise Exception("The access level list is empty.")
 
         # Does the access level exist?
         if lvl_to_delete not in self.access_levels:
-            return False
+            raise Exception(
+                "The access level you are trying to delete does not exist. Please select an existing access level."
+            )
 
         affected_users = self.get_users_with_access_level(lvl_to_delete)
 
@@ -179,11 +205,13 @@ class AccessControlMatrix:
     def delete_user(self, uid):
         # Is the user ID nonempty?
         if not uid:
-            return False
+            raise Exception("The user list is empty.")
 
         # Does the user exist?
         if uid not in self.users:
-            return False
+            raise Exception(
+                "The user you are trying to delete does not exist. Please select an already existing user."
+            )
 
         # Remove from user list and access control
         if uid in self.users:
@@ -193,11 +221,13 @@ class AccessControlMatrix:
     def delete_document(self, fid):
         # Is the document ID nonempty?
         if not fid:
-            return False
+            raise Exception("The document list is empty.")
 
         # Does the document exist?
         if fid not in self.documents:
-            return False
+            raise Exception(
+                "The document you are trying to delete does not exist. Please select an already existing document."
+            )
 
         if fid in self.documents:
             del self.documents[fid]
