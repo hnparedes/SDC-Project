4653c868704640597468d2b04fba022acf745dc8
Rebmiami <rebmiami22@gmail.com>
2026-04-21 19:42:04 -0400

    Fix test_full.py

--- tests/system/test_full.py
+++ tests/system/test_full.py
@@ -94,7 +94,7 @@ def test_full():
 	archiver2.save_draft(draft_path)
 
 	# The user might export the ACM multiple times in one session, and this shouldn't cause problems
-	archiver.export_archive(archive_path, "sonormal")
+	archiver2.export_archive(archive_path, "sonormal")
 
 	# Now that the archive is done, let's test the viewer.
 	viewer = SDCViewer()
