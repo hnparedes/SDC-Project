9d68e232f38bb833693bc52b5957cfe9a7c93cf1
Rebmiami <rebmiami22@gmail.com>
2026-04-21 19:47:37 -0400

    Brought over test SDC regeneration command from main, included regenerated SDC

--- Makefile
+++ Makefile
@@ -1,6 +1,19 @@
+# The specific command to remove files is OS-dependent
+ifeq ($(OS),Windows_NT)
+	delcommand := del
+else
+	delcommand := rm -rf
+endif
+
 # Runs SDC Archiver Program
 run-archiver:
 	uv run ./packages/sdc_archiver/src/sdc_archiver/archiver_ui.py
 # Runs SDC Viewer Program
 run-viewer:
 	uv run ./packages/sdc_viewer/src/sdc_viewer/viewer_ui.py
+# Regenerate test SDC
+regen-test-sdc:
+	uv run ./scripts/regen_test_sdc.py
+# Remove old test files
+clean:
+	$(delcommand) .test-output
---
+++ scripts/regen_test_sdc.py
@@ -0,0 +1,35 @@
+import os.path
+
+from sdc_archiver.archiver_backend import SDCArchiver
+
+test_file_path = "./tests/testfiles/"
+
+# Regenerates normalsdc.7z and normalacm.json
+archiver = SDCArchiver()
+
+archiver.acm.add_access_level("admin")
+archiver.acm.add_access_level("privileged")
+archiver.acm.add_access_level("default")
+
+archiver.acm.add_user("alice", "letmein", "admin")
+archiver.acm.add_user("bob", "opensesame", "privileged")
+archiver.acm.add_user("charles", "password", "default")
+
+archiver.acm.add_document("text_normal_1.txt", ["admin", "privileged", "default"], test_file_path + "files/text_normal_1.txt",)
+archiver.acm.add_document("text_normal_2.txt", ["admin", "privileged"], test_file_path + "files/text_normal_2.txt",)
+archiver.acm.add_document("text_normal_3.txt", ["admin"], test_file_path + "files/text_normal_3.txt",)
+
+# Export draft archive
+draft_output_path = test_file_path + "drafts/"
+os.makedirs(draft_output_path, exist_ok=True)
+
+# This is not implemented yet on this branch
+# draft_path = draft_output_path + "normalacm.json"
+# archiver.save_draft(draft_path)
+
+# Export archive
+archive_output_path = test_file_path + "sdcs/"
+os.makedirs(archive_output_path, exist_ok=True)
+
+archive_path = archive_output_path + "normalsdc.7z"
+archiver.export_archive(archive_path, "sonormal")
